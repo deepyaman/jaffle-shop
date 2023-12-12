7910c269b0be8c017a15705ff01a64f5ec8206d5
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-12-11 20:40:07 -0700

    Add node to create and populate the `orders` table

--- src/jaffle_shop/pipelines/data_processing/nodes.py
+++ src/jaffle_shop/pipelines/data_processing/nodes.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import ibis
 from ibis import _
 
 if TYPE_CHECKING:
@@ -36,4 +37,31 @@ def process_customers(
             _.total_amount.name("customer_lifetime_value"),
         ]
     )
+    return final
+
+
+def process_orders(
+    orders: ir.Table, payments: ir.Table, payment_methods: list[str]
+) -> ir.Table:
+    total_amount_by_payment_method = {}
+    for payment_method in payment_methods:
+        total_amount_by_payment_method[f"{payment_method}_amount"] = ibis.coalesce(
+            payments.amount.sum(where=payments.payment_method == payment_method), 0
+        )
+
+    order_payments = payments.group_by("order_id").aggregate(
+        **total_amount_by_payment_method, total_amount=payments.amount.sum()
+    )
+
+    final = orders.left_join(order_payments, "order_id")[
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+        *[
+            order_payments[f"{payment_method}_amount"]
+            for payment_method in payment_methods
+        ],
+        order_payments.total_amount.name("amount"),
+    ]
     return final
--- src/jaffle_shop/pipelines/data_processing/pipeline.py
+++ src/jaffle_shop/pipelines/data_processing/pipeline.py
@@ -1,6 +1,6 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import process_customers
+from .nodes import process_customers, process_orders
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -10,6 +10,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=process_customers,
                 inputs=["stg_customers", "stg_orders", "stg_payments"],
                 outputs="customers",
-            )
+            ),
+            node(
+                func=process_orders,
+                inputs=["stg_orders", "stg_payments", "params:payment_methods"],
+                outputs="orders",
+            ),
         ]
     )
